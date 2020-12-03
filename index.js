8f9dd9a191eafd6d74f9374904bd65b4ce108cb0
Etash Singh <etash.singh@hpe.com>
2020-12-03 10:54:22 +0530

    initial

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -7,6 +8,16 @@ const server = http.createServer((req, res) => {
   res.end(msg);
 });
 
+const server2 = http.createServer((req, res) => {
+	res.statusCode = 200;
+	const msg2 = 'Hello again from Node!\n'
+	res.end(msg2);
+});
+
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
+
+server2.listen(port2, () => {
+	console.log(`Server2 running on http://localhost:${port2}/`);
+})
